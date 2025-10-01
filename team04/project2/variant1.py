fc5eed4eeb041e2462757db900503c8fa7b90596
Mason Sakai <masonsakai@gmail.com>
2025-09-30 20:05:06 -0400

    Start of project 2

---
+++ team04/project2/variant1.py
@@ -0,0 +1,24 @@
+# This is necessary to find the main code
+import sys
+sys.path.insert(0, '../../bomberman')
+sys.path.insert(1, '..')
+
+# Import necessary stuff
+from game import Game
+
+# TODO This is your code!
+sys.path.insert(1, '../team04')
+
+# Uncomment this if you want the empty test character
+from qlearningcharacter import QLearningCharacter
+
+# Create the game
+g = Game.fromfile('map.txt')
+
+# Uncomment this if you want the test character
+g.add_character(QLearningCharacter("me", # name
+                              "C",   # avatar
+                              0, 0   # position
+))
+
+g.go(1)
